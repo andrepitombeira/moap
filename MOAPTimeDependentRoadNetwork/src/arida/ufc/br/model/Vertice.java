ca5416c2418549f826cdbbca0db0216ee14cff87
Joao Carlos Sousa do Vale <jcarlos.vale@gmail.com>
2013-07-21 10:49:06 -0300

    Atualizacao metodos equals()

--- MOAPTimeDependentRoadNetwork/src/arida/ufc/br/model/Vertice.java
+++ MOAPTimeDependentRoadNetwork/src/arida/ufc/br/model/Vertice.java
@@ -46,11 +46,34 @@ public class Vertice {
 	public double getY() {
 		return y;
 	}
-	
-	public boolean equals(Vertice vertice) {
-		if(vertice.getX() == this.getX() && vertice.getY() == this.getY()) {
-			return true;
-		}
-		return false;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vertice other = (Vertice) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		return true;
+	}
+	
+	
 }
